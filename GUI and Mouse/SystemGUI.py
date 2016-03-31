fa025c703ee933754c6b81b42ebbf45be4db4734
Rachel Hutchinson <Rachel@Rachels-MacBook-Pro-2.local>
2016-03-31 17:03:23 +0100

    Update of GUI system

--- GUI and Mouse/SystemGUI.py
+++ GUI and Mouse/SystemGUI.py
@@ -1,52 +1,102 @@
+from pubsub import pub
 import Tkinter as Tk
 
 ########################################################################
-class MyApp(object):
+class OtherFrame(Tk.Toplevel):
+    """"""
+    
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        Tk.Toplevel.__init__(self)
+        self.geometry("400x300")
+        self.title("otherFrame")
+        
+        # create the button
+        btn = Tk.Button(self, text="Close", command=self.onClose)
+        btn.pack()
+    
+    #----------------------------------------------------------------------
+    def onClose(self):
+        """
+            closes the frame and sends a message to the main frame
+            """
+        self.destroy()
+        pub.sendMessage("otherFrameClosed", arg1="data")
+
+########################################################################
+class StartScreen(object):
     """"""
     
     #----------------------------------------------------------------------
     def __init__(self, parent):
         """Constructor"""
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+    
+        w = 400
+        h = 200
+        x = (screenwidth / 2) - (w / 2)
+        y = (screenheight / 2) - (h / 2)
+    
         self.root = parent
-        self.root.title("Main frame")
-        self.frame = Tk.Frame(parent)
-        self.frame.pack()
+        self.root.title("Welcome")
+        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.root.focus_force()
+        self.frame = Tk.Frame(parent, width = w, height = h, bg = 'red')
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.grid(columnspan = 4)
         
-        btn = Tk.Button(self.frame, text="Open Frame", command=self.openFrame)
-        btn.pack()
+        
+        welcomeLabel = Tk.Label(self.frame, text = "Welcome!", width = 45)
+        calibrateButton = Tk.Button(self.frame, text="Calibrate", command=self.openFrame)
+        quitButton = Tk.Button(self.frame, text="Quit", command=self.quitScreen)
+       
+       #welcomeLabel.columnconfigure(0, weight=1)
+        welcomeLabel.grid(row = 0, columnspan =4)
+        
+        calibrateButton.grid(row = 1, column = 2)
+        quitButton.grid(row = 1, column = 3)
+        
+        pub.subscribe(self.listener, "otherFrameClosed")
+    
+    #----------------------------------------------------------------------
+    def listener(self, arg1, arg2=None):
+        """
+            pubsub listener - opens main frame when otherFrame closes
+            """
+        self.show()
     
     #----------------------------------------------------------------------
     def hide(self):
-        """"""
+        """
+            hides main frame
+            """
         self.root.withdraw()
     
     #----------------------------------------------------------------------
     def openFrame(self):
-        """"""
+        """
+            opens other frame and hides main frame
+            """
         self.hide()
-        otherFrame = Tk.Toplevel()
-        otherFrame.geometry("400x300")
-        otherFrame.title("otherFrame")
-        handler = lambda: self.onCloseOtherFrame(otherFrame)
-        btn = Tk.Button(otherFrame, text="Close", command=handler)
-        btn.pack()
-    
-    #----------------------------------------------------------------------
-    def onCloseOtherFrame(self, otherFrame):
-        """"""
-        otherFrame.destroy()
-        self.show()
+        subFrame = OtherFrame()
     
     #----------------------------------------------------------------------
     def show(self):
-        """"""
+        """
+            shows main frame
+            """
         self.root.update()
         self.root.deiconify()
+    
+    #---------------------------------------------------------------------
+    def quitScreen(self):
+        self.root.destroy()
 
 
 #----------------------------------------------------------------------
 if __name__ == "__main__":
     root = Tk.Tk()
-    root.geometry("800x600")
-    app = MyApp(root)
+    app = StartScreen(root)
     root.mainloop()
