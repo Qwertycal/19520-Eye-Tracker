e1241cd3c9ba3395a2b1d5073b3a2945eca4fe69
unknown <szb11178@eee-r239-05.ds.strath.ac.uk>
2016-04-18 15:48:15 +0100

    Current state of all together code

--- GUI and Mouse/AllTogether.py
+++ GUI and Mouse/AllTogether.py
@@ -4,22 +4,22 @@ import math
 pyautogui.FAILSAFE = False
 speed = 1410
 spaceCount = 0
-cursorClick = 500
+cursorClick = 10
 maxMovement = 10
-
+cursorDoubleClick = 1000
+doubleCount = 0
 width, height = pyautogui.size() #get the width and height of the screen
 
 #List of coordinates to move to
-coords = [(0,0), (492, 383)]
+coords = [(0,0), (492, 383), (0, 500), (600, 10)]
 pointsVisited = []
 pyautogui.moveTo(coords[0], duration=0.25)
 
 var = 1
 loop = 1
 
-while var == 1:
+while var < 20:
     #print (var)
-    pointsVisited.append(pyautogui.position())
     #print (pointsVisited)
     if (loop == 1):
         for i in range (1, len(coords)):
@@ -27,32 +27,42 @@ while var == 1:
             distance = math.sqrt(math.pow((coords[i][0])-(coords[i-1][0]), 2) + math.pow((coords[i][1]) -(coords[i-1][1]),2))
             dur = distance/speed
             print(dur)
-            pyautogui.moveTo(coords[i], duration=dur)
+            pyautogui.moveTo(coords[i], duration=dur); print (pyautogui.position()[0]); print (pyautogui.position()[1])
             pointsVisited.append(pyautogui.position())
             loop += 1
-    
+	else:
+		pointsVisited.append(pyautogui.position())
     #If the cursor has been in the same sort of location for a while, then click
-    if (i>(cursorClick - 1)):
+    if len(pointsVisited) > cursorClick:
+		prevPos = len(pointsVisited) - 1
         #Look at the last 4th previous move, set a box, 10 pixels wide around the cursors location at this move.
-        lowBound1 = (pointsVisited[i-cursorClick][0])-maxMovement
-        lowBound2 = (pointsVisited[i-cursorClick][1])-maxMovement
-        highBound1 = (pointsVisited[i-cursorClick][0])+maxMovement
-        highBound2 = (pointsVisited[i-cursorClick][1])+maxMovement
-        spaceCount = 0
+		lowBound1 = (pointsVisited[prevPos-cursorClick][0])-maxMovement
+		lowBound2 = (pointsVisited[prevPos-cursorClick][1])-maxMovement
+		highBound1 = (pointsVisited[prevPos-cursorClick][0])+maxMovement
+		highBound2 = (pointsVisited[prevPos-cursorClick][1])+maxMovement
+		spaceCount = 0
         
+		print 'Bounds'
+		print lowBound1
+		print lowBound2
+		print highBound1
+		print highBound2
+		
         #For all the moves between the 4th previous move and the current one, check if the cursor has stayed within the bounds, if it has for each then add one to spaceCount
-        for j in range((i-(cursorClick - 1)), (i+1)):
+		for j in range((prevPos-(cursorClick - 1)), (prevPos+1)):
             #print ('j' + str(j))
             #print('spaceCount: ' + str(spaceCount))
-            if((lowBound1<= pointsVisited[j][0] <= highBound1) & (lowBound2) <= pointsVisited[j][1] <= (highBound2)):
-                spaceCount += 1
-            else:
-                spaceCount = 0
+			if((lowBound1<= pointsVisited[j][0] <= highBound1) & (lowBound2) <= pointsVisited[j][1] <= (highBound2)):
+				print 'Points visited current'
+				print pointsVisited[j][0]
+				print pointsVisited[j][1]
+				spaceCount += 1
+			else:
+				spaceCount = 0
     #If there have been 4 consecutive moves in a row within the bounds, then click, and check if there should be a scroll
     if (spaceCount == cursorClick):
         print('Click')
-        pyautogui.click(clicks = 2)
-        pyautogui.doubleClick();
+        pyautogui.click()
         spaceCount = 0
         #If the cursor is near a scroll bar, then scroll, up or down
         scrollUpLoc = pyautogui.locateOnScreen('scrollUp.png')
@@ -70,7 +80,8 @@ while var == 1:
                 print ("Implement scroll down")
                 pyautogui.scroll(5)
                 print("Scrolled down")
-    i += 1
+	i += 1
+	var += 1
 
 
 
--- GUI and Mouse/AllTogetherTest.py
+++ GUI and Mouse/AllTogetherTest.py
@@ -21,33 +21,35 @@ width, height = pyautogui.size() #get the width and height of the screen
 pointsVisited = []
 pointsClicked = []
 
-def move_mouse(x1,y1):
+var = 1
+
+while var == 1:
     spaceCount = 0
-    cursorClick = 80 #alters how long the user must look at a point before a click
-    maxMovement = 200 #alters the area the user must look at to invoke a click
+    cursorClick = 200 #alters how long the user must look at a point before a click
+    maxMovement = 10 #alters the area the user must look at to invoke a click
     #maxClickMovement = 200
-    cursorDoubleClick = 160
+    cursorDoubleClick = 400
     doubleCount = 0
     
     #Move to the first location and add the location to the list
-    if len(pointsVisited) <=0:
-        pyautogui.moveTo((x1,y1), duration=0)
-        pointsVisited.append(pyautogui.position())
+    # if len(pointsVisited) <=0:
+        #pyautogui.moveTo((x1,y1), duration=0)
+    pointsVisited.append(pyautogui.position())
     
     #For the next locations find the previous location, work out the distance between
     #the previous location and the one to move to and set the time this should take
     #Move to the current location for the set amount of time, and add the current
     #location to the list
-    else:
-        prevPos = len(pointsVisited) - 1
-        point = pointsVisited[prevPos]
-        oldX = point[0]
-        oldY = point[1]
+    # else:
+    prevPos = len(pointsVisited) - 1
+        # point = pointsVisited[prevPos]
+        # oldX = point[0]
+        # oldY = point[1]
         #For each set of coordinates in the list, move to the coordinates, at an appropriate speed.
-        distance = math.sqrt(math.pow(x1-oldX, 2) + math.pow(y1 -oldY,2))
-        dur = distance/speed
-        pyautogui.moveTo((x1,y1), duration=dur)
-        pointsVisited.append(pyautogui.position())
+        # distance = math.sqrt(math.pow(x1-oldX, 2) + math.pow(y1 -oldY,2))
+        # dur = distance/speed
+        # pyautogui.moveTo((x1,y1), duration=dur)
+        # pointsVisited.append(pyautogui.position())
 
 #    print 'len points visited'
 #    print (len(pointsVisited))
@@ -56,39 +58,66 @@ def move_mouse(x1,y1):
     if len(pointsVisited) > cursorClick:
         #Look at the first location in the range (cursorClick) set a box, (maxMovement) pixels wide around
         #the cursors location at this point.
-        lowBound1 = (pointsVisited[prevPos-cursorClick][0])-maxMovement
-        lowBound2 = (pointsVisited[prevPos-cursorClick][1])-maxMovement
-        highBound1 = (pointsVisited[prevPos-cursorClick][0])+maxMovement
-        highBound2 = (pointsVisited[prevPos-cursorClick][1])+maxMovement
-        #spaceCount = 0
+		lowBound1 = (pointsVisited[prevPos-cursorClick][0])-maxMovement
+		lowBound2 = (pointsVisited[prevPos-cursorClick][1])-maxMovement
+		highBound1 = (pointsVisited[prevPos-cursorClick][0])+maxMovement
+		highBound2 = (pointsVisited[prevPos-cursorClick][1])+maxMovement
+		
+		print 'Bounds'
+		print lowBound1
+		print lowBound2
+		print highBound1
+		print highBound2
+		spaceCount = 0
         #For all the moves between the (cursorClick)th previous move and the current one,
         #check if the cursor has stayed within the bounds, if it has for each then add one to spaceCount
-        for j in range((prevPos-(cursorClick - 1)), (prevPos+1)):
-            if((lowBound1<= pointsVisited[j][0] <= highBound1) & (lowBound2) <= pointsVisited[j][1] <= (highBound2)):
-                spaceCount += 1
-            else:
-                spaceCount = 0
-        if len(pointsVisited) > cursorDoubleClick:
-            for k in range((prevPos-(cursorDoubleClick - 1)), (prevPos+1)):
-                if((lowBound1<= pointsVisited[k][0] <= highBound1) & (lowBound2) <= pointsVisited[k][1] <= (highBound2)):
-                    doubleCount += 1
+		for j in range((prevPos-(cursorClick - 1)), (prevPos+1)):
+			print spaceCount
+			if((lowBound1<= pointsVisited[j][0] <= highBound1) & (lowBound2) <= pointsVisited[j][1] <= (highBound2)):
+				print 'Points visited current'
+				print pointsVisited[j][0]
+				print pointsVisited[j][1]
+				spaceCount += 1
+			else:
+				spaceCount = 0
+		if len(pointsVisited) > cursorDoubleClick:
+			for k in range((prevPos-(cursorDoubleClick - 1)), (prevPos+1)):
+				if((lowBound1<= pointsVisited[k][0] <= highBound1) & (lowBound2) <= pointsVisited[k][1] <= (highBound2)):
+					doubleCount += 1
                 else:
                     doubleCount = 0
-#        print doubleCount
-    print spaceCount
+    #print doubleCount
+    
     #If there have been (cursorClick) consecutive moves in a row within the bounds, then click, and check if there should be a scroll
-    if (doubleCount == cursorDoubleClick):
-        print 'double click'
-        pyautogui.click(clicks = 2)
-        doubleCount = 0
-        spaceCount = 0
 
     if (spaceCount == cursorClick):
-        print('Click Invoked')
-        #if len(pointsClicked) <= cursorDoubleClick:
-        pyautogui.click()
-        pointsClicked.append(pyautogui.position())
-        spaceCount = 0
+		if (doubleCount == cursorDoubleClick):
+			print 'double click'
+			pyautogui.doubleClick()
+			doubleCount = 0
+			spaceCount = 0
+		else:
+			print('Click Invoked')
+			#if len(pointsClicked) <= cursorDoubleClick:
+			pyautogui.click()
+			pointsClicked.append(pyautogui.position())
+			spaceCount = 0
+		#If the cursor is near a scroll bar, then scroll, up or down
+		scrollUpLoc = pyautogui.locateOnScreen('scrollUp.png')
+		print('scrollUpLoc' + str(scrollUpLoc))
+		scrollDownLoc = pyautogui.locateOnScreen('scrollDown.png')
+		print('scrollDownLoc' + str(scrollDownLoc))
+		x, y = pyautogui.position()
+		if (scrollUpLoc != None):
+			if (x > (scrollUpLoc[0] - 5) and x < (scrollUpLoc[0] + scrollUpLoc[2] + 5) and y > (scrollUpLoc[1] - 5) and y < (scrollUpLoc[1] + scrollUpLoc [3] + 5)):
+				print ("Implement scroll up")
+				pyautogui.scroll(5)
+				print("Scrolled up")
+		if (scrollDownLoc != None):
+			if (x > (scrollDownLoc[0] - 5) and x < (scrollDownLoc[0] + scrollDownLoc[2] + 5) and y > (scrollDownLoc[1] - 5) and y < (scrollDownLoc[1] + scrollDownLoc [3] + 5)):
+				print ("Implement scroll down")
+				pyautogui.scroll(5)
+				print("Scrolled down")
 #        print 'Point Clicked'
 #        print pointsClicked[0][0]
 #        print pointsClicked[0][1]
